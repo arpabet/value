173af66e2616a00a3f9c21eb4e281d640a1c91db
John Delacion <john@mailnite.com>
2019-02-09 23:37:10 -0800

    add benchmark test

--- value_test.go
+++ value_test.go
@@ -24,9 +24,12 @@ import (
 	"github.com/stretchr/testify/require"
 	"bytes"
 	"time"
+	"runtime"
+	"fmt"
 )
 
 const numIterations = 10
+const numBenchIterations = 10000
 
 func testPackUnpack(t *testing.T, v val.Value) {
 
@@ -76,4 +79,62 @@ func TestStream(t *testing.T) {
 	require.Equal(t, numIterations, cnt)
 
 
+}
+
+func TestBenchmark(t *testing.T) {
+
+	m := testCreateMap()
+
+	runtime.GC()
+	tnow := time.Now()
+
+	buf := bytes.Buffer{}
+	p := val.MessagePacker(&buf)
+
+	for i:=0; i!=numBenchIterations;i++ {
+		m.Pack(p)
+	}
+
+	encDur := time.Now().Sub(tnow)
+	encLen := len(buf.Bytes())
+	runtime.GC()
+
+	unpacker := val.MessageUnpacker(buf.Bytes(), false)
+	parser := val.MessageParser()
+
+	tnow = time.Now()
+	for i:=0; i!=numBenchIterations;i++ {
+		val.Parse(unpacker, parser)
+	}
+
+	decDur := time.Now().Sub(tnow)
+
+	fmt.Printf("Benchmark %d ops, encode_len=%d, encode_duration=%v, decode_duration=%v\n", numBenchIterations, encLen, encDur, decDur)
+
+	writeBs := int64(time.Second / encDur) * numBenchIterations
+	readBs := int64(time.Second / decDur) * numBenchIterations
+
+	fmt.Printf("Throughput write=%v bs, read=%v bs\n", writeBs, readBs)
+
+
+}
+
+func testCreateMap() val.Table {
+
+	b := val.Map()
+
+	b.Insert(val.Boolean(true))
+	b.Insert(val.Long(123))
+	b.Insert(val.Double(-12.34))
+	b.Insert(val.Utf8("text"))
+	b.Insert(val.Raw([]byte{0, 1, 2}, false))
+
+	c := val.Map()
+	c.Put("5", val.Long(5))
+
+	b.Put("name", val.Utf8("name"))
+	b.Put("123", val.Long(123))
+	b.Put("map", c)
+
+	return b
 }
